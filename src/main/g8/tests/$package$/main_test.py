d4710766dabcec3da2a66b53fadb55651518819f
phrmoy <phrmoy@gmail.com>
2023-01-27 17:37:45 -0300

    still testing this out

---
+++ src/main/g8/tests/$package$/main_test.py
@@ -0,0 +1,17 @@
+"""
+Tests for $package$.main.py
+"""
+
+from $package$.main import *
+
+
+class MainTest:
+    """
+    MainTest
+    """
+
+    def automated_main_test(self):
+        """
+        MainTest
+        """
+        assert True
